aae22391d66e54ddf383341ec94a99e2a5c70804
lmikszta1 <lmikszta@gmail.com>
2023-06-05 09:47:55 -0400

    Part 1-4 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+    
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Fed on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
+
